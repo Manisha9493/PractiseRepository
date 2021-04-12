317eda3c4a85b8147389ceb998734579f52953cf
manisha <manisha.dibs@gmail.com>
2021-04-12 11:15:15 +0530

    Commiting Good morning message

--- PracticeRepository/src/com/pr/beans/Test.java
+++ PracticeRepository/src/com/pr/beans/Test.java
@@ -5,7 +5,7 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("helooo world");
 		System.out.println("hiii Manisha");
-		System.out.println("Good Morning");
+		System.out.println("Good Morning have a nice day");
 
 	}
 
